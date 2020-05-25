2a0a828ae06f104041964e2d5d8abd1c04996b09
ydjopa <ydjopa@gmail.com>
2020-05-25 15:46:50 -0400

    jscript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#owl-demo").owlCarousel({
+        autoPlay: 3000,
+        stopOnHover: true,
+        navigation: true,
+        paginationSpeed: 1000,
+        goToFirstSpeed: 2000,
+        signleItem: true,
+        autoHeight: true,
+        transitionStyle: "fade"
+    });
+});
